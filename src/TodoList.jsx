460f86b63b4c5721d5fd9c264bdd79b458d7fa63
John Larson <jl@buildmidwestern.com>
2024-02-09 00:32:27 -0600

    fix: disabled prop type validations

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,13 +1,20 @@
-import { TodoItem } from "./TodoItem"
+/* eslint-disable react/prop-types */
+import { TodoItem } from "./TodoItem";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
-    return (
+  return (
     <ul className="list">
       {todos.length === 0 && "No Todos"}
-      {todos.map(todo => {
-        return <TodoItem {...todo} key={todo.id} toggleTodo=
-        {toggleTodo} deleteTodo={deleteTodo}/>
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        );
       })}
-  </ul>
- )
+    </ul>
+  );
 }
